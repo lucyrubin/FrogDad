d16b7abc5c6e2cb036d412e201f59fa2c34f0391
Lilly Lu <98109844+lilly1u@users.noreply.github.com>
2023-03-20 23:29:56 -0500

    Updated artwork

--- FrogDad0.4/FrogDad0.4/Scripts/Home.gd
+++ FrogDad0.4/FrogDad0.4/Scripts/Home.gd
@@ -8,6 +8,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$YSort/Door.z_index = $YSort/Door.position.y - 40
 	$YSort/Dresser.z_index = $YSort/Dresser.position.y - 40
 	$"YSort/TV Stand".z_index = $"YSort/TV Stand".position.y
 	$YSort/Cradle.z_index = $YSort/Cradle.position.y - 40
@@ -16,5 +17,4 @@ func _ready():
 	$YSort/Couch.z_index = $YSort/Couch.position.y
 	$YSort/Table.z_index = $YSort/Table.position.y - 20
 	$YSort/Chairs.z_index = $YSort/Chairs.position.y - 40
-	$YSort/Door.z_index = $YSort/Door.position.y
-	
+	$YSort/Window.z_index = $YSort/Window.position.y
--- FrogDad0.4/FrogDad0.4/Scripts/Sink.gd
+++ FrogDad0.4/FrogDad0.4/Scripts/Sink.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
-#var current_fram = 0
-#var num_frames = 4
+var current_fram = 0
+var num_frames = 2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,6 +16,5 @@ func _on_Sink_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton \
 	and event.button_index == BUTTON_LEFT \
 	and event.pressed:
-		#current_fram = (current_fram +1) % num_frames
-		$AnimatedSprite.play("default")
-		#$AnimatedSprite.set_frame(current_fram)
+		current_fram = (current_fram +1) % num_frames
+		$AnimatedSprite.set_frame(current_fram)
