c749297894aee1c4e80975a6527504cf6394e60f
lucyrubin <lucyjr111@gmail.com>
2023-03-26 15:14:41 -0500

    fixed some bugs

--- FrogDad/Scripts/Home.gd
+++ FrogDad/Scripts/Home.gd
@@ -4,5 +4,5 @@ extends Node2D
 func _ready(): 
 	# set the z-index of all objects to be based on the y position
 	for object in $YSort.get_children():
-		object.z_index = object.position.y - 40
+		object.z_index = object.position.y- 40
 		
