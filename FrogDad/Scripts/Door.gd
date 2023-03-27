d61d1afb58d9c95296a67c18dd177422ebfdb7bf
Lilly Lu <98109844+lilly1u@users.noreply.github.com>
2023-03-27 11:18:59 -0500

    Door mechanics works

--- FrogDad/Scripts/Door.gd
+++ FrogDad/Scripts/Door.gd
@@ -1,19 +1,28 @@
 extends Area2D
 
 export (PackedScene) var target_scene = load("res://Scenes/Outdoors.tscn")
-
+var door_opened = false
+var current_fram = 0
+var num_frames = 2
 
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
-		print(get_overlapping_bodies().size())
 		if !target_scene:
 			print("no scene in this door")
-			
-		if get_overlapping_bodies().size()>1:
+		if door_opened:
 			go_outside()
-			
 
 func go_outside():
 		var ERR = get_tree().change_scene_to(target_scene)
 		if ERR != OK:
 			print("something failed in the door scene")
+
+func _on_Door_area_entered(area):
+	door_opened = true
+	current_fram = (current_fram +1) % num_frames
+	$AnimatedSprite.set_frame(current_fram)
+
+func _on_Door_area_exited(area):
+	door_opened = false
+	current_fram = (current_fram-1) % num_frames
+	$AnimatedSprite.set_frame(current_fram)
--- FrogDad/Scripts/OutdoorDoor.gd
+++ FrogDad/Scripts/OutdoorDoor.gd
@@ -1,14 +1,13 @@
 extends Area2D
 
 export (PackedScene) var target_scene = load("res://Scenes/Main.tscn")
+var door_opened
 
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
-		print(get_overlapping_bodies().size())
 		if !target_scene:
 			print("no scene in this door")
-			
-		if get_overlapping_bodies().size()>0:
+		if door_opened:
 			go_outside()
 			#this works for now but worried it may cause issues in future
 
@@ -16,3 +15,11 @@ func go_outside():
 		var ERR = get_tree().change_scene_to(target_scene)
 		if ERR != OK:
 			print("something failed in the door scene")
+
+
+func _on_OutdoorDoor_area_entered(area):
+	door_opened = true
+
+
+func _on_OutdoorDoor_area_exited(area):
+	door_opened = false
