3091b780adab1fcb4f3faa8f0f2240855fb77550
helenazhu <hzhu@macalester.edu>
2023-04-03 10:41:11 -0500

    Fixed one DBox issue.

--- FrogDad/Scripts/DialogueBox2.gd
+++ FrogDad/Scripts/DialogueBox2.gd
@@ -1,16 +1,53 @@
 extends CanvasLayer
 
+const AVATAR_MAP = {
+	"gertrude": preload("res://Temporary Clipart/gertrude copy (1).png"),
+	"frogDad": preload("res://Temporary Clipart/frog_dad_icon.png")
+}
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export var interval = 0.1 # interval between when each character shows up
+
+var dialogs = []
+var current = 0
+
+onready var content = $Content
+onready var avatar = $Content/Avatar
+
+onready var tween = $Content/Tween
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	hide_dialog_box()
+	show_dialog_box([
+		{avatar = "gertrude", text = "Hi, Frog Dad!"},
+		{avatar = "frogDad", text = "Hi,Gertrude!"},
+		{avatar = "getrude", text = "What are we doing today?"},
+		{avatar = "frogDad", text = "Whatever you wish :)"}
+	])
+	
+func _unhandled_input(event):
+	# if user pressed "space key" before text animation ends,
+	# the animation would be skipped and all text would show
+	if event.is_action_pressed("ui_accept"):
+		if current + 1 < dialogs.size():
+			_show_dialog(current + 1)
+		else:
+			hide_dialog_box()
+		get_tree().set_input_as_handled()
+
+func hide_dialog_box():
+	content.hide()
+	
+func show_dialog_box(_dialogs):
+	dialogs = _dialogs
+	content.show()
+	_show_dialog(0)
+	
+func _show_dialog(index):
+	current = index
+	var dialog = dialogs[current]
+	content.text = dialog.text
+	avatar.texture = AVATAR_MAP[dialog.avatar]
+	
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
