6092ee001428c8327033c97539389209b75ba15b
Lilly Lu <98109844+lilly1u@users.noreply.github.com>
2023-04-09 15:55:44 -0500

    Changed going into forest

--- FrogDad/Scenes/ForestEntrance.gd
+++ FrogDad/Scenes/ForestEntrance.gd
@@ -4,15 +4,6 @@ extends Area2D
 
 export (PackedScene) var target_scene = load("res://Scenes/Forest.tscn")
 
-var in_forest
-
-func _input(event):
-	if event.is_action_pressed("open"):
-		if !target_scene:
-			print("no scene in this door")
-		if in_forest:
-			go_in_forest()
-
 func go_in_forest():
 	MasterScript.enter_forest = true
 	var ERR = get_tree().change_scene_to(target_scene)
@@ -21,8 +12,4 @@ func go_in_forest():
 
 
 func _on_ForestEntrance_area_entered(area):
-	in_forest = true
-
-
-func _on_ForestEntrance_area_exited(area):
-	in_forest = false
+	go_in_forest()
--- FrogDad/Scenes/ForestExit.gd
+++ FrogDad/Scenes/ForestExit.gd
@@ -2,14 +2,6 @@ extends Area2D
 
 export (PackedScene) var target_scene = load("res://Scenes/Outdoors.tscn")
 
-var forest_exit_area = false
-
-func _input(event):
-	if event.is_action_pressed("open") && forest_exit_area:
-		if !target_scene:
-			print("no scene in this door")
-		leave_forest()
-
 func leave_forest():
 	MasterScript.exit_forest = true
 	var ERR = get_tree().change_scene_to(target_scene)
@@ -18,7 +10,4 @@ func leave_forest():
 
 
 func _on_ForestExit_area_entered(area):
-	forest_exit_area = true
-
-func _on_ForestExit_area_exited(area):
-	forest_exit_area = false
+	leave_forest()
