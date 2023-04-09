e3ca01f4dc3e31e2dc259e069a6cad4708a17015
Lilly Lu <98109844+lilly1u@users.noreply.github.com>
2023-04-09 14:12:37 -0500

    Forest scene made

--- FrogDad/Scenes/ForestExit.gd
+++ FrogDad/Scenes/ForestExit.gd
@@ -2,8 +2,10 @@ extends Area2D
 
 export (PackedScene) var target_scene = load("res://Scenes/Outdoors.tscn")
 
+var forest_exit_area = false
+
 func _input(event):
-	if event.is_action_pressed("open"):
+	if event.is_action_pressed("open") && forest_exit_area:
 		if !target_scene:
 			print("no scene in this door")
 		leave_forest()
@@ -13,3 +15,10 @@ func leave_forest():
 	var ERR = get_tree().change_scene_to(target_scene)
 	if ERR != OK:
 		print("something failed in the door scene")
+
+
+func _on_ForestExit_area_entered(area):
+	forest_exit_area = true
+
+func _on_ForestExit_area_exited(area):
+	forest_exit_area = false
--- FrogDad/Scenes/Outdoors.gd
+++ FrogDad/Scenes/Outdoors.gd
@@ -1,9 +1,15 @@
 extends Node2D
 
 func _ready():
+	for object in $YSort.get_children():
+		object.z_index = object.position.y + 20
+	
 	if MasterScript.exit_home:
 		$FrogDad.position = $ExitHouse.position
 		MasterScript.exit_home = false
 	if MasterScript.exit_forest:
 		$FrogDad.position = $ExitForest.position
 		MasterScript.exit_forest = false
+
+func _process(delta):
+	$FrogDad.z_index = ($FrogDad.position.y)
