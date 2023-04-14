6629fa0be8446891dfde6004903f5958649ba8d1
Jaylynn Rodriguez <70664722+jrworm@users.noreply.github.com>
2023-04-13 23:57:47 -0500

    Update FindBabiesCutScene.gd

--- FrogDad/Scenes/FindBabiesCutScene.gd
+++ FrogDad/Scenes/FindBabiesCutScene.gd
@@ -10,7 +10,7 @@ func _on_Timer_timeout():
 	# this code creates and displays a dialogue box	
 	$SmallPopUpBox.visible = true
 	$SmallPopUpBox.show_dialog_box([{avatar = "", text = "Wow, someone trusted me with their babies? I want to help them, but they look so small and fragile. How should I carry them?"},
-							{avatar = "", text = "I should go grab some cloth and make a swaddle."}], "Wow, someone trusted me with their babies?")
+							{avatar = "", text = "I should go grab some cloth and make a swaddle. I think I have some around the house..."}], "Wow, someone trusted me with their babies?")
 	# this code creates and displays a dialogue box	
 	$Timer.queue_free()
 	
