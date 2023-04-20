d4c58fcae780a54906d9328cf15182cff1955a6e
lucyrubin <lucyjr111@gmail.com>
2023-04-20 09:52:38 -0500

    reversed lettuce animation order

--- FrogDad/Scripts/Plot.gd
+++ FrogDad/Scripts/Plot.gd
@@ -1,15 +1,24 @@
 extends Panel
 
-var current_fram = 0
+#var current_fram = 0
+var current_fram = 3
 var num_frames = 4
 
 
 func _on_Plot_gui_input(event):
+#	if event is InputEventMouseButton: 
+#		if event.button_index == BUTTON_LEFT and event.pressed:
+#			current_fram = (current_fram + 1)
+#			if current_fram == 4:
+#				$AnimatedSprite.visible = false
+#				$ItemDrop.visible = true
+#			else:
+#				$AnimatedSprite.set_frame(current_fram)
 	if event is InputEventMouseButton: 
-		if event.button_index == BUTTON_LEFT and event.pressed:
-			current_fram = (current_fram + 1)
-			if current_fram == 4:
-				$AnimatedSprite.visible = false
-				$ItemDrop.visible = true
-			else:
-				$AnimatedSprite.set_frame(current_fram)
+			if event.button_index == BUTTON_LEFT and event.pressed:
+				current_fram = (current_fram - 1)
+				if current_fram == 0:
+					$AnimatedSprite.visible = false
+					$ItemDrop.visible = true
+				else:
+					$AnimatedSprite.set_frame(current_fram)
