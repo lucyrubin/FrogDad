b082110a91d02af690d40dac6ba2682d8c5c0a08
lucyrubin <lucyjr111@gmail.com>
2023-04-25 13:18:11 -0500

    lettuce forest has flies :)

--- FrogDad/Scripts/Fly.gd
+++ FrogDad/Scripts/Fly.gd
@@ -1,13 +1,16 @@
 extends KinematicBody2D
 
-var velocity
 
-var rng = RandomNumberGenerator.new()
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
 
+
+# Called when the node enters the scene tree for the first time.
 func _ready():
-	velocity = Vector2.ZERO
-	
-func _process(delta):
-	velocity.x += rng.randf_range(-100, 100.0)
-	velocity.y += rng.randf_range(-100, 100.0)
-	move_and_slide(velocity * delta)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
