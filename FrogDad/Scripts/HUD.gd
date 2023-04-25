c729b8eec6427bd28d50df97c2722e1dd0bba576
Lilly Lu <98109844+lilly1u@users.noreply.github.com>
2023-04-25 16:01:41 -0500

    Replaced old pop up note with new pop up note

--- FrogDad/Scripts/HUD.gd
+++ FrogDad/Scripts/HUD.gd
@@ -2,30 +2,18 @@ extends CanvasLayer
 
 onready var pause_menu = $PauseMenu
 
-
 func new_quest(quest_text):
-	$PopUpNotification/TextureRect/HBoxContainer/QuestNameLabel.text = quest_text
+	$PopUpNotification/HBoxContainer/QuestNameLabel.text = quest_text
 	$PopUpNotification.visible = true
 	$PopUpNotification/AnimationPlayer.current_animation = "slide_in"
 	$PopUpNotification/AnimationPlayer.play()
-
 	$PopUpNotification/PopUpTimer.start()
 
-
-
-
 func _on_PopUpTimer_timeout():
-
 	$PopUpNotification/AnimationPlayer.current_animation = "fade_out"
 	$PopUpNotification/AnimationPlayer.play()
-	
 	yield($PopUpNotification/AnimationPlayer,'animation_finished')
-	
 	$PopUpNotification/AnimationPlayer.stop()
-
-
-
-
 
 func _on_PauseButton_pressed():
 	pause_menu.show_menu()
