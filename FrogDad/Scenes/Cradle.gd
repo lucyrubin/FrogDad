0b78d70d7ae9e9ec682a67239751c7186978295b
Lilly Lu <98109844+lilly1u@users.noreply.github.com>
2023-05-01 23:19:20 -0500

    Made cradle its own script

--- FrogDad/Scenes/Cradle.gd
+++ FrogDad/Scenes/Cradle.gd
@@ -1,6 +1,8 @@
 extends StaticBody2D
 
-# sets z index of the object so that the object can be moved without messing up the perspective
 func _ready():
 	var texture_height = $Sprite.texture.get_height()
 	z_index = global_position.y - (4 * texture_height / 3)
+	if MasterScript.currentQuestNum < 2:
+		visible = false
+		$CollisionShape2D.set_deferred("disabled", true)
