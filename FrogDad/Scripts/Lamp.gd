1872c143b995a612b63be710b74c978a3283bf5b
Lilly Lu <98109844+lilly1u@users.noreply.github.com>
2023-05-01 23:01:06 -0500

    Lamp hovers, works with cursor, and has sound

--- FrogDad/Scripts/Lamp.gd
+++ FrogDad/Scripts/Lamp.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 var current_fram = 0
 var num_frames = 2
+var in_area
 
 func _on_Lamp_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton \
@@ -10,3 +11,24 @@ func _on_Lamp_input_event(_viewport, event, _shape_idx):
 			current_fram = (current_fram +1) % num_frames
 			$AnimatedSprite.set_frame(current_fram)
 			$Light2D.visible = !$Light2D.visible
+			$Switch.play()
+
+func _on_Lamp_area_entered(area):
+	if MasterScript.frog_dad_state == "":
+		in_area = true
+
+func _on_Lamp_area_exited(area):
+	if MasterScript.frog_dad_state == "":
+		in_area = false
+
+func _on_Lamp_mouse_entered():
+	if MasterScript.frog_dad_state == "" and in_area:
+		$AnimatedSprite.animation = "hover"
+		$AnimatedSprite.set_frame(current_fram)
+		Input.set_custom_mouse_cursor(MasterScript.pointer)
+
+func _on_Lamp_mouse_exited():
+	if MasterScript.frog_dad_state == "":
+		$AnimatedSprite.animation = "default"
+		$AnimatedSprite.set_frame(current_fram)
+		Input.set_custom_mouse_cursor(MasterScript.hand)
