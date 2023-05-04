2b233e3c5d55ede92561532763492669fdb3995c
Lilly Lu <98109844+lilly1u@users.noreply.github.com>
2023-05-04 08:23:56 -0500

    Cleaned Main script

--- FrogDad/Scripts/Main.gd
+++ FrogDad/Scripts/Main.gd
@@ -7,12 +7,16 @@ func _ready():
 	$FrogDad/AnimatedSprite.animation = "down"
 	$FrogDad/AnimatedSprite.frame = 0
 	MasterScript.frog_dad_state = ""
+	_handle_itemdrop_visibility()
+	
 	if MasterScript.enter_home:
 		$FrogDad.position = $EnterHome.position
 		MasterScript.enter_home = false
+
 	if MasterScript.findBabies:
 		$FrogDad.position = $EnterHome.position
 		MasterScript.findBabies = false
+
 	if MasterScript.enter_cradle_area:
 		$FrogDad.position = $EnterCradle.position
 		$FrogDad.toggle_dialogue_box_visibility([{avatar = "", text = "Phew! It ain't much, but it's honest work!"},
@@ -21,10 +25,54 @@ func _ready():
 		$Home/Cradle.visible = true
 		$Home/Jar.visible = true
 		$Home/Cradle/CollisionShape2D.disabled = false
-		
+
 	if MasterScript.after_eggs_to_tadpoles:
 		$FrogDad.position = $EnterCradle.position
 		MasterScript.after_eggs_to_tadpoles = false
+
+	if MasterScript.currentQuestNum < 2:
+		_hide_babies()
+	elif MasterScript.currentQuestNum >= 2 and  MasterScript.currentQuestNum < 6: 
+		$Home/Jar.visible = false
+		_show_babies()
+		$Home/Cradle/CollisionShape2D.disabled = false
+		get_tree().get_root().find_node("Cradle", true, false).visible = true
+		if MasterScript.enter_cradle_area:
+			$Home/Jar.visible = true
+			$BabyGilbert.visible = false
+			MasterScript.enter_cradle_area = false
+	else:
+		$Home/Jar.visible = false
+		_hide_babies()
+		get_tree().get_root().find_node("Cradle", true, false).visible = true
+		$Home/Cradle/CollisionShape2D.disabled = false
+
+func _process(_delta):
+	$FrogDad.z_index = $FrogDad.position.y # these two lines handle layers to that things that are higher on the screen are 
+	$BabyGertrude.z_index = $BabyGertrude.position.y - 10 # behind things lower on the screen
+	$BabyGilbert.z_index = $BabyGilbert.position.y + 5
+	$BabyGravyBaby.z_index = $BabyGravyBaby.position.y +40
+
+func drop_item(item_drop : ItemDropClass):
+	item_drop.setup($FrogDad/AnimatedSprite.global_position.x + 100, $FrogDad/AnimatedSprite.global_position.y + 100, item_drop.item_name, $FrogDad/UserInterface/Inventory)
+	add_child(item_drop)
+
+func add_item_drop(type, x, y):
+	var drop = ItemDropScene.instance()
+	drop.setup(x,y,type, $FrogDad/UserInterface/Inventory)
+	add_child(drop)
+	
+func _show_babies():
+	$BabyGertrude.visible = true
+	$BabyGilbert.visible = true
+	$BabyGravyBaby.visible = true
+
+func _hide_babies():
+	$BabyGertrude.visible = false
+	$BabyGilbert.visible = false
+	$BabyGravyBaby.visible = false
+
+func _handle_itemdrop_visibility():
 	if MasterScript.currentQuestNum != 0:
 		$ItemDrop5.hide()
 		$ItemDrop6.hide()
@@ -33,41 +81,3 @@ func _ready():
 		$ItemDrop5.show()
 		$ItemDrop6.show()
 		$ItemDrop7.show()
-	if MasterScript.currentQuestNum < 2:
-		$BabyGertrude.visible = false
-		$BabyGilbert.visible = false
-		$BabyGravyBaby.visible = false
-	elif MasterScript.currentQuestNum >= 2 and  MasterScript.currentQuestNum < 6: 
-		$Home/Jar.visible = false
-		$BabyGertrude.visible = true
-		$BabyGilbert.visible = true
-		$BabyGravyBaby.visible = true
-		$Home/Cradle/CollisionShape2D.disabled = false
-		get_tree().get_root().find_node("Cradle", true, false).visible = true
-		if MasterScript.enter_cradle_area:
-			$Home/Jar.visible = true
-			$BabyGilbert.visible = false
-			MasterScript.enter_cradle_area = false
-	else:
-		$BabyGertrude.visible = false
-		$BabyGilbert.visible = false
-		$BabyGravyBaby.visible = false
-		get_tree().get_root().find_node("Cradle", true, false).visible = true
-		$Home/Cradle/CollisionShape2D.disabled = false
-		$Home/Jar.visible = false
-	
-
-func _process(_delta):
-	$FrogDad.z_index = ($FrogDad.position.y) # these two lines handle layers to that things that are higher on the screen are 
-	$BabyGertrude.z_index = $BabyGertrude.position.y - 10 # behind things lower on the screen
-	$BabyGilbert.z_index = $BabyGilbert.position.y + 5
-	$BabyGravyBaby.z_index = $BabyGravyBaby.position.y +40
-
-func drop_item(item_drop : ItemDropClass):
-	item_drop.setup($FrogDad/AnimatedSprite.global_position.x + 100,$FrogDad/AnimatedSprite.global_position.y + 100,item_drop.item_name, $FrogDad/UserInterface/Inventory )
-	add_child(item_drop)	
-
-func add_item_drop(type, x, y):
-	var drop = ItemDropScene.instance()
-	drop.setup(x,y,type, $FrogDad/UserInterface/Inventory)
-	add_child(drop)
