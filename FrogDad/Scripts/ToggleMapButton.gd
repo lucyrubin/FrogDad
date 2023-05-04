afc0bce40bc04672668c3d3fbc833747e2bb06e7
Lilly Lu <98109844+lilly1u@users.noreply.github.com>
2023-05-04 08:05:13 -0500

    Fixed map button

--- FrogDad/Scripts/ToggleMapButton.gd
+++ FrogDad/Scripts/ToggleMapButton.gd
@@ -6,10 +6,10 @@ extends TextureButton
 func _on_ToggleMapButton_mouse_entered():
 	if MasterScript.frog_dad_state == "":
 		Input.set_custom_mouse_cursor(MasterScript.pointer)
-		texture_normal = load("res://Art/todo_icon_hover.png")
+		texture_normal = load("res://Info Icons/map icon hover.png")
 
 
 func _on_ToggleMapButton_mouse_exited():
 	if MasterScript.frog_dad_state == "":
 		Input.set_custom_mouse_cursor(MasterScript.hand)
-		texture_normal = load("res://Art/todo_icon.png")
+		texture_normal = load("res://Info Icons/map icon normal.png")
