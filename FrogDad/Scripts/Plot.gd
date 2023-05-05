4f210e4f1f9ac6dc777132ecc6bd133f54103d8a
lucyrubin <lucyjr111@gmail.com>
2023-05-04 19:38:19 -0500

    lettuce particles

--- FrogDad/Scripts/Plot.gd
+++ FrogDad/Scripts/Plot.gd
@@ -12,10 +12,20 @@ func _on_Plot_gui_input(event):
 		if event is InputEventMouseButton: 
 				if event.button_index == BUTTON_LEFT and event.pressed:
 					current_fram = (current_fram - 1)
-					if current_fram == 0:
+					if current_fram == -1:
+						$CPUParticles2D.queue_free()
+					elif current_fram == 0:
+						if get_node("CPUParticles2D"):
+							$CPUParticles2D.restart()
+							$CPUParticles2D.emitting = true
+							$CPUParticles2D.position.y += 100
 						$AnimatedSprite.visible = false
 						$ItemDrop.visible = true
 					else:
+						if get_node("CPUParticles2D"):
+							$CPUParticles2D.restart()
+							$CPUParticles2D.emitting = true
+							$CPUParticles2D.position.y += 100
 						$AnimatedSprite.set_frame(current_fram)
 
 func _on_Plot_mouse_entered():
